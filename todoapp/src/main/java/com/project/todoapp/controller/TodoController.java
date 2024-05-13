a945e71ef233dae2cb64bb29883b19fea825df7e
Shyam Sreenivasan <shyamsreeni.612@gmail.com>
2024-05-13 19:46:05 +0530

    added update and delete endpoints

--- todoapp/src/main/java/com/project/todoapp/controller/TodoController.java
+++ todoapp/src/main/java/com/project/todoapp/controller/TodoController.java
@@ -1,15 +1,20 @@
 package com.project.todoapp.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.todoapp.model.Todo;
+import com.project.todoapp.model.TodoStatus;
 import com.project.todoapp.repository.TodoRepository;
 
 
@@ -34,6 +39,36 @@ public class TodoController {
         todoRepository.save(todo);
         return ResponseEntity.created(null).build();
     }
-    
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateTodo(@PathVariable Integer id, @RequestBody Todo todoToUpdate) {
+        Optional<Todo> optionalTodo = todoRepository.findById(id);
+        if (optionalTodo.isPresent()) {
+            Todo todo = optionalTodo.get();
+            if (!todoToUpdate.getName().isEmpty()) {
+                todo.setName(todoToUpdate.getName());
+            }
+            TodoStatus statusToChange = todoToUpdate.getStatus();
+            for (TodoStatus status : TodoStatus.values()) {
+                if (status.equals(statusToChange)) {
+                    todo.setStatus(status);
+                }
+            }                
+            todoRepository.save(todo);
+            return ResponseEntity.ok().build();
+        }
+        
+        return ResponseEntity.notFound().build();
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable Integer id) {
+        Optional<Todo> optionalTodo = todoRepository.findById(id);
+        if (optionalTodo.isPresent()) {
+                Todo todo = optionalTodo.get();
+                todoRepository.delete(todo);
+                return ResponseEntity.ok().build();
+            }
+        return ResponseEntity.notFound().build();
+    }
 }
